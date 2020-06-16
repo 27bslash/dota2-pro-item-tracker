b510206348cc5d5f893ff68c59be42e69216b0e7
Callum Kay <greenpear68@yahoo.co.ukcode - helpcode --helpcode --help>
2020-06-16 20:07:15 +0100

    optimised get request for pro tracker

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 import os
 import requests
 from flask import Flask, render_template, request, url_for, redirect
+from flask_caching import Cache
 from opendota_api import *
 from parsel import Selector
 import time
 import string
 app = Flask(__name__)
+cache = Cache(config={
+    # 'CACHE_TYPE': 'NULL'
+})
+cache.init_app(app)
 
 
 @app.route('/', methods=['GET'])
@@ -17,8 +22,10 @@ def hello():
 def post_req():
     if request.method == 'POST':
         text = request.form.get('t')
-        if get_id(text) >= 0:
-            print('hero')
+        vale = request.form.get('toggle_start')
+        print(text, get_id(text))
+        if get_id(text):
+            print('hero', get_id(text))
             return redirect('/hero/'+text)
         else:
             print('invalid hero')
@@ -26,8 +33,11 @@ def post_req():
 
 
 @app.route('/hero/<hero_name>')
+@cache.cached(timeout=60)
 def show_items(hero_name):
+    print('g')
     do_everything(hero_name)
+    print('afsdg')
     entries = []
     with open('opendota_output.json', 'r') as f:
         data = json.load(f)
@@ -36,31 +46,32 @@ def show_items(hero_name):
         print('show-items', hero_name)
         for data_i in data:
             entries.append(data_i['hero'])
-        return render_template('final_items.html', hero_name=hero_name, data=data, time=90, entries=entries)
+        return render_template('final_items.html', hero_name=hero_name, data=data, time=90)
 
 
-@ app.route('/hero/<hero_name>', methods=['POST', 'GET'])
+@app.route('/hero/<hero_name>', methods=['POST', 'GET'])
 def redirect_page(hero_name):
     if request.method == 'POST':
         text = request.form.get('t')
-        print('id test', get_id(text))
-        if get_id(text) >= 0:
-            print('sdfg')
-            return redirect('/hero/'+text)
-        else:
-            print('invalid hero')
-            return render_template('hero.html.jinja')
+        vlaue = request.form
+        print('id test', hero_name, get_id(text), False >= 0)
+        return redirect('/hero/'+text)
+
+
+@app.route('/test')
+def re():
+    return 'sdfakl'
 
 
 def do_everything(hero_name):
     output = []
-    # request_shit(hero_name, output)
-    get_item_name(50)
+    amount = 10
     start = time.time()
-    asyncio.run(main(get_urls(3), hero_name))
+    asyncio.run(pro_request(hero_name, output, amount))
+    asyncio.run(main(get_urls(amount), hero_name))
     delete_output()
     end = time.time()
-    print("Took {} seconds to pull {} websites.".format(end - start, 5))
+    print("Took {} seconds to pull {} websites.".format(end - start, amount))
 
 
 def is_num(s):
@@ -71,24 +82,36 @@ def is_num(s):
         return False
 
 
-def request_shit(hero_name, output):
+async def request_shit(hero_name, output, amount):
+    start = time.time()
     hero_name = hero_name.title()
     hero_name = hero_name.replace(' ', '%20')
-    url = 'https://www.dota2protracker.com/hero/'+hero_name
-    req = requests.get(url)
-    print(req.status_code)
-    text = req.text
-    selector = Selector(text=text)
-    table = selector.xpath('//*[@class="display compact"]//tbody//tr')
-    for row in table:
-        match_id = row.css('a::attr(href)').re(r".*opendota.*")[0]
-        mmr = row.xpath('td')[4].css('::text').extract()[0]
-        print(match_id, mmr)
-        if match_id:
-            o = [{'id': match_id}, {'mmr': mmr}]
-            output.append(o)
-    with open('test.json', 'w') as outfile:
-        json.dump(output, outfile)
+    url = 'http://www.dota2protracker.com/hero/'+hero_name
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url=url) as response:
+            req = await response.text()
+            text = req
+            # f = open('test.html', 'w')
+            # f.write(text)
+            # f.close()
+            selector = Selector(text=text)
+            table = selector.xpath('//*[@class="display compact"]//tbody//tr')
+            for row in table:
+                match_id = row.css('a::attr(href)').re(r".*opendota.*")[0]
+                mmr = row.xpath('td')[4].css('::text').extract()[0]
+                if match_id and len(output) < amount:
+                    print(match_id, mmr)
+                    o = [{'id': match_id}, {'mmr': mmr}]
+                    output.append(o)
+                with open('test.json', 'w') as outfile:
+                    json.dump(output, outfile)
+            end = time.time()
+            print(end-start, 'seconds')
+            
+
+
+async def pro_request(hero_name, output, amount):
+    ret = await asyncio.gather(request_shit(hero_name, output, amount))
 
 
 if __name__ == '__main__':
--- opendota_api.py
+++ opendota_api.py
@@ -4,7 +4,8 @@ import asyncio
 import aiohttp
 import datetime
 import traceback
-
+from collections import OrderedDict
+from operator import itemgetter
 # def get_hero_name(name):
 #     global hero_name
 #     hero_name = name
@@ -30,45 +31,20 @@ async def async_get(url, hero_name):
                             # check if one of the players matches search
                             purchase_log = p['purchase_log']
                             if purchase_log:
-                                for item in purchase_log:
-                                    # change item purchase time to readable time
-                                    if item['time'] > 0:
-                                        item['time'] = str(datetime.timedelta(
-                                            seconds=item['time']))
-                                        item['time'] == ['time']
-                                    else:
-                                        item['time'] = 0
                                 final_items = []
-                                sorted_final_items = []
-
                                 backpack = []
+                                starting_items = []
+                                for purchase in purchase_log:
+                                    if purchase['time'] <= 0:
+                                        starting_items.append(
+                                            {'key': purchase['key'], 'time': 0})
                                 rev = purchase_log.copy()[::-1]
-                                for j in range(6):
-                                    # main items
-                                    item_str = 'item_'+str(j)
-                                    item = get_item_name(p[item_str])
-                                    for purchase in rev:
-                                        if purchase['key'] == item:
-                                            if len(final_items) < 10:
-                                                final_items.append(
-                                                    {'key': item, 'time': purchase['time']})
-                                for j in range(4):
-                                    # back pack items
-                                    item_str = 'backpack_'+str(j)
-                                    item = get_item_name(p[item_str])
-                                    for purchase in rev:
-                                        if purchase['key'] == item:
-                                            if len(final_items) < 10:
-                                                final_items.append(
-                                                    {'key': item, 'time': purchase['time']})
-
-                                
-                                # sorting reeeeeeeeeeeeeeeeeeeeeeeee
-                                # for j in range(len(final_items)):
-                                #     for k in sorted(final_items[j]):
-                                #         print(k ,final_items[j][k])
+                                main_items = get_most_recent_items(
+                                    rev, 6, final_items, p)
+                                bp_items = get_most_recent_items(
+                                    rev, 4, backpack, p)
                     output.append(
-                        {'hero': hero_name, 'id': resp['match_id'], 'final_items': final_items, 'items': purchase_log})
+                        {'hero': hero_name, 'id': resp['match_id'], 'starting_items': starting_items, 'final_items': main_items, 'backpack': bp_items, 'items': purchase_log})
                     json.dump(output, outfile, indent=4)
     except Exception as e:
         print("Unable to get url {} due to {}.".format(
@@ -79,16 +55,56 @@ async def main(urls, hero_name):
     ret = await asyncio.gather(*[async_get(url, hero_name) for url in urls])
 
 
+def sort_dict(items):
+    newlist = sorted(
+        items, key=itemgetter('time'))
+    for item in newlist:
+        if item['time'] > 0:
+            item['time'] = str(datetime.timedelta(
+                seconds=item['time']))
+            item['time'] == ['time']
+        else:
+            item['time'] = 0
+    return newlist
+
+
+def get_most_recent_items(arr, l, out, p):
+    done = set()
+    for j in range(l):
+        if l == 6:
+            item_str = 'item_'+str(j)
+        else:
+            item_str = 'backpack_'+str(j)
+        item = get_item_name(p[item_str])
+        for purchase in arr:
+            if purchase['key'] == item and purchase['key'] not in done:
+                if len(out) < 10:
+                    done.add(purchase['key'])
+                    out.append(
+                        {'key': item, 'time': purchase['time']})
+    return sort_dict(out)
+
+
 def delete_output():
     global output
     output = []
+
+
+def delete_dupes(d):
+    done = set()
+    result = []
+    for item in d:
+        if item['key'] not in done:
+            done.add(item['key'])  # note it down for further iterations
+            result.append(item)
+    return result
 
 
 def get_urls(amount):
     urls = []
     with open("test.json") as json_file:
         data = json.load(json_file)
-        for i in range(10):
+        for i in range(amount):
             try:
                 m_id = data[i][0]['id']
                 m_id = re.sub(r"www", 'api', m_id)
@@ -101,14 +117,17 @@ def get_urls(amount):
 
 
 def get_id(name):
-    with open('hero_ids.json') as json_file:
-        data = json.load(json_file)
-        for i in range(len(data['heroes'])):
-            name = name.lower()
-            name = name.replace(' ', '_')
-            # print(name)
-            if name in data['heroes'][i]['name']:
-                return data['heroes'][i]['id']
+    if name:
+        with open('hero_ids.json') as json_file:
+            data = json.load(json_file)
+            for i in range(len(data['heroes'])):
+                name = name.lower()
+                name = name.replace(' ', '_')
+                if name in data['heroes'][i]['name']:
+                    return data['heroes'][i]['id']
+            return False
+    else:
+        return False
 
 
 def get_item_name(item_id):
