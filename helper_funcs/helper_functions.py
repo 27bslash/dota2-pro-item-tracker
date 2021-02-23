20f8a1258ed8af3cf85e9adada4ada1e061210b8
27bslash <greenpear68@yahoo.co.uk>
2021-02-23 16:43:32 +0000

    moved switcher function

--- helper_funcs/helper_functions.py
+++ helper_funcs/helper_functions.py
@@ -412,6 +412,34 @@ def detailed_ability_info(arr, h_id):
     return output
 
 
+def switcher(name):
+    switch = {
+        'necrophos': 'necrolyte',
+        'clockwerk': 'rattletrap',
+        "nature's_prophet": 'furion',
+        'timbersaw': 'shredder',
+        'io': 'wisp',
+        'queen_of_pain': 'queenofpain',
+        'doom': 'doom_bringer',
+        'shadow_fiend': 'nevermore',
+        'wraith_king': 'skeleton_king',
+        'magnus': 'magnataur',
+        'underlord': 'abyssal_underlord',
+        'anti-mage': 'antimage',
+        'outworld_devourer': 'obsidian_destroyer',
+        'windranger': 'windrunner',
+        'zeus': 'zuus',
+        'vengeful_spirit': 'vengefulspirit',
+        'treant_protector': 'treant',
+        'centaur_warrunner': 'centaur'
+    }
+    # print(h, switch.get(h))
+    if switch.get(name):
+        return switch.get(name)
+    else:
+        return name
+
+
 ab_arr = [654
           ]
 if __name__ == "__main__":
