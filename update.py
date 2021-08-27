aaca0fbd06d3ac1cd3c2c56e1d03482c9e3ffcf4
27bslash <greenpear68@yahoo.co.uk>
2021-08-27 15:33:08 +0100

    changed update to add to db

--- update.py
+++ update.py
@@ -4,35 +4,36 @@ from colorthief import ColorThief
 from helper_funcs.helper_imports import *
 from colours.contrast import compute_contrast
 from colours.dominant_colour import get_dominant_colours, get_dominant_color
-from accounts.download_acount_ids import get_account_ids
+from accounts.download_acount_ids import update_pro_accounts
 import shutil
 import requests
 import traceback
 
+hero_list = db['hero_list'].find_one({}, {'_id': 0})
+
 
 def update_hero_list():
-    with open(f"json_files/hero_ids.json", 'w') as f:
-        data = json.loads(requests.get(
-            'https://api.stratz.com/api/v1/Hero').text)
-        hero_dict = {'heroes': []}
-        for k in data:
-            hero_name = data[k]['shortName']
-            hero_id = int(k)
-            hero_dict['heroes'].append({'name': hero_name, 'id': hero_id})
+    data = json.loads(requests.get(
+        'https://api.stratz.com/api/v1/Hero').text)
+    hero_dict = {'heroes': []}
+    for k in data:
+        hero_name = data[k]['shortName']
+        hero_id = int(k)
+        hero_dict['heroes'].append({'name': hero_name, 'id': hero_id})
 
-        json.dump(hero_dict, f, indent=4)
+    db['hero_list'].find_one_and_update(
+        {}, {"$set": hero_dict},upsert=True)
 
 
-def upload_hero_list():
-    with open('json_files/hero_ids.json', 'r') as f:
-        data = json.load(f)
-        db['hero_list'].find_one_and_update(
-            {}, {"$set": {'heroes': data['heroes']}})
+# def upload_hero_list():
+#     with open('json_files/hero_ids.json', 'r') as f:
+#         data = json.load(f)
+#         db['hero_list'].find_one_and_update(
+#             {}, {"$set": {'heroes': data['heroes']}})
 
 
 def make_dir():
     if not os.path.isdir('colours\\ability_images'):
-        print('fg')
         os.mkdir('colours\\ability_images')
     for filename in os.listdir("colours\\ability_images"):
         filepath = os.path.join("colours\\ability_images", filename)
@@ -40,31 +41,28 @@ def make_dir():
             shutil.rmtree(filepath)
         except OSError:
             os.remove(filepath)
-    with open('json_files/hero_ids.json', 'r') as d:
-        data = json.load(d)
-        for item in data['heroes']:
-            hero_name = item['name']
-            os.mkdir(f"colours/ability_images/{hero_name}")
+    for hero in hero_list['heroes']:
+        hero_name = hero['name']
+        os.mkdir(f"colours/ability_images/{hero_name}")
 
 
 def dl_dota2_abilities():
     make_dir()
     datafeed = 'https://www.dota2.com/datafeed/herodata?language=english&hero_id='
-    with open('json_files/hero_ids.json', 'r') as heroes:
-        hero_data = json.load(heroes)
-        for hero in hero_data['heroes']:
-            req = requests.get(f"{datafeed}{hero['id']}")
-            with open(f'json_files/detailed_ability_info/{hero["name"]}.json', 'w') as o:
-                ability_json = json.loads(
-                    req.text)['result']['data']['heroes'][0]
-                hero_abilities = {}
-                print(hero['name'])
-                for ability in ability_json['abilities']:
-                    hero_abilities[str(ability['id'])] = ability
-                    get_ability_img(ability['name'], hero['name'])
-                for talent in ability_json['talents']:
-                    hero_abilities[str(talent['id'])] = talent
-                json.dump(hero_abilities, o, indent=4)
+    for hero in hero_list['heroes']:
+        req = requests.get(f"{datafeed}{hero['id']}")
+        ability_json = json.loads(
+            req.text)['result']['data']['heroes'][0]
+        hero_abilities = {}
+        print(hero['name'])
+        for ability in ability_json['abilities']:
+            hero_abilities[str(ability['id'])] = ability
+            get_ability_img(ability['name'], hero['name'])
+        for talent in ability_json['talents']:
+            hero_abilities[str(talent['id'])] = talent
+        db['individual_abilities'].find_one_and_update(
+            {'hero': hero['name']}, {"$set": {"abilities": hero_abilities}}, upsert=True)
+        # json.dump(hero_abilities, o, indent=4)
 
 
 def get_ability_img(ability_name, hero_name):
@@ -91,39 +89,50 @@ def chunk_stratz_abilites():
                     json.dump(d, w, indent=4)
 
 
-def update_stratz_json():
-    with open(f"json_files/stratz_talents.json", 'w') as f:
-        f.write(json.dumps(json.loads(requests.get(
-            'https://api.stratz.com/api/v1/Hero').text), indent=4))
-    with open(f"json_files/stratz_abilities.json", 'w') as f:
-        f.write(json.dumps(json.loads(requests.get(
-            'https://api.stratz.com/api/v1/Ability').text), indent=4))
-    with open(f"json_files/stratz_items.json", 'w') as f:
-        f.write(json.dumps(json.loads(requests.get(
-            'https://api.stratz.com/api/v1/Item').text), indent=4))
-    update_basic_id_json('stratz_items', 'items', 'items')
+def update_stratz_json(url, collection):
+    req = requests.get(url).json()
+    db_data = db[collection].find_one({}, {'_id': 0})
+    if not db_data or not req == db_data and len(req) > len(db_data):
+        db[collection].find_one_and_update(
+            {}, {"$set": req}, upsert=True)
+
+    update_basic_id_json('all_items', 'item_ids', 'items')
+    # req = requests.get('https://api.stratz.com/api/v1/Hero').json()
+    # b = req == json.load(open('json_files/stratz_talents.json'))
+    # if not b and len(req) > 2000:
+    #     db[collection].find_one_and_update(
+    #         {}, {"$set": {'talents': req}}, upsert=True)
+
+    # req = requests.get('https://api.stratz.com/api/v1/Item').json()
+    # b = req == json.load(open('json_files/stratz_abilities.json'))
+    # if not b and len(req) > 2000:
+    #     db['all_items'].find_one_and_update(
+    #         {}, {"$set": {'items': req}}, upsert=True)
+    # with open(f"json_files/stratz_talents.json", 'w') as f:
+    #     f.write(json.dumps(json.loads(requests.get(
+    #         'https://api.stratz.com/api/v1/Hero').text), indent=4))
+    # with open(f"json_files/stratz_abilities.json", 'w') as f:
+    #     f.write(json.dumps(json.loads(requests.get(
+    #         'https://api.stratz.com/api/v1/Ability').text), indent=4))
+    # with open(f"json_files/stratz_items.json", 'w') as f:
+    #     f.write(json.dumps(json.loads(requests.get(
+    #         'https://api.stratz.com/api/v1/Item').text), indent=4))
 
 
-def update_basic_id_json(input, output, dic_name):
+def update_basic_id_json(input_collection, output_collection, dic_name):
     dictionary = {dic_name: []}
-    with open(f"json_files/{input}.json", 'r') as f:
-        data = json.load(f)
-        for _id in data:
-            try:
-                dictionary[dic_name].append(
-                    {'name': data[_id]['shortName'], "id": data[_id]['id']})
-            except:
-                print(traceback.format_exc(), data[_id]['shortName'])
-    with open(f'json_files/{output}.json', 'w') as f:
-        json.dump(dictionary, f, indent=2)
+    data = db[input_collection].find_one({}, {'_id': 0})
+    for _id in data:
+        dictionary[dic_name].append(
+            {'name': data[_id]['shortName'], "id": data[_id]['id']})
+    db[output_collection].find_one_and_update(
+        {}, {"$set": dictionary}, upsert=True)
 
 
 def update_talents():
     db_methods = Db_insert()
-    with open("json_files/hero_ids.json", 'r') as f:
-        data = json.load(f)
-        for hero in data['heroes']:
-            db_methods.insert_talent_order(hero['id'])
+    for hero in hero_list['heroes']:
+        db_methods.insert_talent_order(hero['id'])
 
 
 def update_minimap_icons():
@@ -139,9 +148,12 @@ def update_minimap_icons():
 
 def update_app():
     print('uploading hero list')
-    upload_hero_list()
+    update_hero_list()
     print('updating json....')
-    update_stratz_json()
+    update_stratz_json('https://api.stratz.com/api/v1/Item', 'all_items')
+    update_stratz_json('https://api.stratz.com/api/v1/Hero', 'all_talents')
+    update_stratz_json(
+        'https://api.stratz.com/api/v1/Ability', 'all_abilities')
     print('downloading abilities...')
     dl_dota2_abilities()
     print('updating_talents...')
@@ -151,11 +163,16 @@ def update_app():
     print('updating minimap icons...')
     update_minimap_icons()
     print('updating account ids')
-    get_account_ids()
+    update_pro_accounts()
     print('fini')
 
 
 if __name__ == '__main__':
+    # update_stratz_json('https://api.stratz.com/api/v1/Item', 'all_items')
+    # update_stratz_json('https://api.stratz.com/api/v1/Hero', 'all_talents')
+    # update_stratz_json(
+    #     'https://api.stratz.com/api/v1/Ability', 'all_abilities')
+    # dl_dota2_abilities()
     update_app()
     # chunk_stratz_abilites()
     # update_hero_list()
