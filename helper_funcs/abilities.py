3b66b0d27bab1e9f58db06ba725f7b528f1809b2
27bslash <greenpear68@yahoo.co.uk>
2021-10-02 15:05:55 +0100

    removed all _abilities db

--- helper_funcs/abilities.py
+++ helper_funcs/abilities.py
@@ -1,11 +1,8 @@
 import json
 import traceback
-from collections import Counter, OrderedDict
 from helper_funcs.hero import Hero, db
-
+from helper_funcs.switcher import switcher
 hero_methods = Hero()
-all_abilities = db['all_abilities'].find_one({}, {'_id': 0})
-all_talents = db['all_talents'].find_one({}, {'_id': 0})
 # def get_hero_name_from_ability(ability_list):
 #     for ability in ability_list:
 #         with open('json_files/stratz_abilities.json', 'r') as f:
@@ -35,48 +32,49 @@ def detailed_ability_info(ability_list, hero_id):
     output = []
     talents = []
     # print(len(stat_count))
+    hero_name = switcher(hero_methods.hero_name_from_hero_id(hero_id))
     st_count = 0
     temp_st_count = 0
     gap = 0
+    data = db['individual_abilities'].find_one({'hero': hero_name})
+    abilities = data['abilities']
+    talents = data['talents']
+    all_abilities = {**abilities, **talents}
     for i, _id in enumerate(ability_list):
         _id = str(_id)
-        if _id in all_abilities:
-            if int(_id) == 730:
-                # print(i,gap)
-                st_count += 1
-                temp_st_count += 1
-                continue
-            # print(i+1,gap,i+1+gap)
-            # print(i+1 + gap,i, data[_id]['language']['displayName'])
+        if int(_id) == 730:
+            # print(i,gap)
+            st_count += 1
+            temp_st_count += 1
+            continue
+        elif _id in all_abilities:
             try:
                 d = {}
                 d['img'] = all_abilities[_id]['name']
-                d['key'] = all_abilities[_id]['language']['displayName']
+                d['key'] = all_abilities[_id]['name_loc']
                 d['id'] = _id
-                # invoker edge case
+
+                if _id in talents:
+                    d['type'] = 'talent'
+                    for k in talents:
+                        if _id == k:
+                            d['slot'] = talents[k]['slot']
+                else:
+                    d['type'] = 'ability'
                 if hero_id != 74:
                     gap = skill_gap(gap, _id, temp_st_count,
-                                    st_count, level=i+1)
+                                    st_count, i+1, d['type'])
                 d['level'] = i+1+gap
-                if 'special_bonus' in all_abilities[_id]['name']:
-                    d['type'] = 'talent'
-                    talents = db['individual_abilities'].find_one(
-                        {'hero': hero_methods.hero_name_from_hero_id(hero_id)}, {'_id': 0, 'talents': 1})
-                    for k in talents['talents']:
-                        if _id == k:
-                            d['slot'] = talents['talents'][k]['slot']
-                            # update_talents_db(hero_id, d, _id)
-                else: 
-                    d['type'] = 'ability'
                 output.append(d)
                 if hero_id != 74:
+                    # invoker edge case
                     output = output[slice(0, 19)]
             except Exception as e:
                 print(_id, traceback.format_exc())
     return output
 
 
-def skill_gap(gap, _id, temp_st_count, st_count, level):
+def skill_gap(gap, _id, temp_st_count, st_count, level, type):
     if level+gap == 17:
         if temp_st_count == 0:
             gap += 1
@@ -90,51 +88,14 @@ def skill_gap(gap, _id, temp_st_count, st_count, level):
     if level+gap == 20:
         pass
     if level+gap > 20:
-        if 'special_bonus' in all_abilities[_id]['name'] and st_count > 0:
+        if type == 'talent' and st_count > 0:
             gap += 25 - level
         if 25 - level + gap == temp_st_count:
-            if 'special_bonus' in all_abilities[_id]['name']:
+            if type == 'talent':
                 gap += temp_st_count
         if st_count == 0:
             gap += 4
     return gap
-
-
-class Talents():
-    def __init__(self):
-        self.db = db
-
-    def count_talents(self, data):
-        try:
-            talents = [ability['key']
-                       for item in data for ability in item['abilities'] if 'special_bonus' in ability['img']]
-            # print('counter',talents,dict(Counter(talents)))
-            return dict(Counter(talents))
-        except Exception as e:
-            print('y', traceback.format_exc())
-
-    def get_talent_order(self, match_data, hero):
-        talents = []
-        count = self.count_talents(match_data)
-        if count is None:
-            return False
-        talents = self.db['talents'].find_one({'hero': hero})
-        for x in talents['talents']:
-            if x['key'] in count:
-                x['talent_count'] = count[x['key']]
-            else:
-                x['talent_count'] = 0
-        level = 10
-        for i in range(0, 8, 2):
-            if i < 8:
-                picks = talents['talents'][i]['talent_count'] + \
-                    talents['talents'][i+1]['talent_count']
-                talents['talents'][i]['total_pick_count'] = picks
-                talents['talents'][i+1]['total_pick_count'] = picks
-                talents['talents'][i]['level'] = level
-                talents['talents'][i+1]['level'] = level
-                level += 5
-        return list(reversed(talents['talents']))
 
 
 ab_arr = [
