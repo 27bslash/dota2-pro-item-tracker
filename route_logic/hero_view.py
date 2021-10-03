57dfb7fd20a1c28b16b49e083034c939ca7b449c
27bslash <greenpear68@yahoo.co.uk>
2021-10-03 19:12:03 +0100

    removed match data from init page load

--- route_logic/hero_view.py
+++ route_logic/hero_view.py
@@ -23,23 +23,15 @@ class HeroView(View):
                                          request=request, player='')
         pick_data = db['wins'].find_one({'hero': hero_name}, {'_id': 0, 'Hard Support_picks': 1, 'Support_picks': 1,
                                                               'Roaming_picks': 1, 'Offlane_picks': 1, 'Midlane_picks': 1, 'Safelane_picks': 1})
-        # roles_db = db['hero_picks'].find_one({'hero': hero_name})
-        # roles = sorted(pick_data, key=itemgetter(1), reverse=True)
         roles = {k: pick_data[k] for k in sorted(
             pick_data, key=pick_data.get, reverse=True)}
         total = hero_output.count_documents({'hero': hero_name})
         check_response = hero_output.find_one({'hero': hero_name})
         if check_response:
             best_games = View.role(self, hero_name, request)['best_games']
-            match_data = View.role(self, hero_name, request)['match_data']
-            most_used = item_methods.pro_items(match_data)
-            most_used = dict(itertools.islice(most_used.items(), 10))
-            max_val = list(most_used.values())[0]
-            talents = talent_methods.get_talent_order(match_data, hero_name)
-            talent_img = [order_talents(talents, i) for i in range(10, 30, 5)]
             hero_colour = self.get_hero_name_colour(hero_name)
-            return {'template': template, 'max': max_val, 'most_used': most_used, 'hero_img': hero_name, 'display_name': display_name, 'hero_name': switcher(hero_name), 'data': match_data,
-                    'time': time.time(), 'total': total, 'talents': talents, 'talent_img': talent_img, 'hero_colour': hero_colour, 'roles': roles, 'best_games': best_games}
+            return {'template': template, 'hero_img': hero_name, 'display_name': display_name, 'hero_name': switcher(hero_name),
+                    'time': time.time(), 'total': total, 'hero_colour': hero_colour, 'roles': roles, 'best_games': best_games}
         else:
             return {'template': template, 'hero_name': hero_name, 'hero_img': hero_name, 'display_name': display_name, 'data': [], 'time': time.time(), 'total': 0, 'hero_colour': get_hero_name_colour(hero_name), 'roles': roles}
 
--- route_logic/view.py
+++ route_logic/view.py
@@ -11,18 +11,11 @@ class View():
     def role(self, name, request):
         if request.args:
             role = request.args.get('role').replace('%20', ' ').title()
-            data = hero_output.find(
-                {'hero': name, 'role': role})
-            match_data = [hero for hero in data]
             best_games = [match for match in db['best_games'].find(
                 {'hero': name, 'display_role': role})]
         else:
-            match_data = self.find_hero('hero', name)
             best_games = [match for match in db['best_games'].find(
                 {'hero': name, 'display_role': None})]
-        return {'match_data': match_data, 'best_games': best_games}
+        return {'best_games': best_games}
 
-    def find_hero(self, query, hero):
-        data = hero_output.find({query: hero})
-        match_data = [hero for hero in data]
-        return match_data
+
