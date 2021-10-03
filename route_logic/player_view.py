89968b559140fe490087fe442c7c748497fd17f0
27bslash <greenpear68@yahoo.co.uk>
2021-10-03 23:07:25 +0100

    fixed match data in player view

--- route_logic/player_view.py
+++ route_logic/player_view.py
@@ -11,7 +11,6 @@ class PlayerView(View):
 
     def player_view(self, player_name: str, request):
         player_name = switcher(player_name)
-        print(player_name)
         template = View.templateSelector(self,
                                          request=request, player='player_')
         display_name = player_name.replace('%20', ' ')
@@ -20,7 +19,6 @@ class PlayerView(View):
         total = roles_db['total_picks']
         check_response = hero_output.find_one({'name': player_name})
         if check_response:
-            match_data = View.role(self, player_name, request)['match_data']
-            return {'template': template, 'display_name': display_name, 'data': match_data, 'time': time.time(), 'total': total, 'role_total': len(match_data), 'roles': roles}
+            return {'template': template, 'display_name': display_name,  'time': time.time(), 'total': total, 'roles': roles}
         else:
             return {'template': template, 'display_name': display_name, 'data': [], 'time': time, 'roles': roles, 'total': 0}
