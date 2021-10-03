f310a62cae8449cc7311ff95fea4e414414f8bb8
27bslash <greenpear68@yahoo.co.uk>
2021-10-03 19:11:39 +0100

    split table into smaller funcs

--- helper_funcs/table.py
+++ helper_funcs/table.py
@@ -30,8 +30,8 @@ def generate_table(func_name, query, template, request):
         data = hero_output.find(
             {key: query, 'role': role}).sort(column, sort_direction).limit(length).skip(records_to_skip)
         match_data = [match for match in data]
-        total_entries = [entry for entry in hero_output.count_documents(
-            {key: query, 'role': role})]
+        total_entries = hero_output.count_documents(
+            {key: query, 'role': role})
     else:
         if len(searchable) > 0 and len(search_value) > 0:
             aggregate = {key: query, 'hero': {"$in": search_value}}
@@ -43,87 +43,21 @@ def generate_table(func_name, query, template, request):
         total_entries = hero_output.count_documents({key: query})
     result = {"draw": request.args['draw'],
               "recordsTotal": total_entries, "recordsFiltered": total_entries, "data": []}
-    if total_entries== 0:
+    if total_entries == 0:
         return result
+
+    return generate_table_string(func_name, match_data, template, result)
+
+
+def generate_table_string(func_name, match_data, template, result):
     img_host = 'https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/'
     for match in match_data:
         row_string = []
         html_string = f"<a href=https://www.opendota.com/matches/{match['id']}>"
-        html_string += "<div class='purchases'>"
-        if 'start' in template:
-            html_string += item_text_str(match['starting_items'],
-                                         img_host, 'starter', match['hero'])
-            html_string += "</div>"
-            html_string += "<div class='intermediate_items'>"
-            for item in match['items']:
-                intermediate_items = ['bottle', 'vanguard', 'hood_of_defiance', 'orb_of_corrosion',
-                                      'soul_ring', 'buckler', 'urn', 'fluffy_hat', 'wind_lace', 'infused_raindrop', 'crown', 'bracer', 'null_talisman', 'wraith_band',
-                                      'ring_of_basilius', 'headress', 'magic_wand']
-                consumables = ['tango', 'flask', 'ward_observer',
-                               'ward_sentry', 'smoke_of_deceit', 'enchanted_mango', 'clarity', 'tpscroll', 'dust']
-                item_key = item['key']
-                item_id = item_methods.get_item_id(item_key)
-                if type(item['time']) is not int or item['time'] > 600:
-                    break
-                if item['key'] not in consumables and item['time'] < 600 and item['time'] > 0:
-                    image = f"<img class='item-img' src='{img_host}/items/{item['key']}.png' data_id='{item_id}' alt='{item_key}'>"
-                    overlay = f"<div class='overlay'>{str(datetime.timedelta(seconds=item['time']))}</div>"
-                    html_string += "<div class='item-cell'>"
-                    html_string += image
-                    html_string += overlay
-                    html_string += "<div class='tooltip' id='item-tooltip'></div>"
-                    html_string += "</div>"
-            html_string += "</div></a>"
-
-        else:
-            html_string += item_text_str(match['final_items'],
-                                         img_host, None, match['hero'])
-            html_string += item_text_str(match['backpack'],
-                                         img_host, None, match['hero'])
-
-            if match['item_neutral']:
-                item_key = match['item_neutral']
-                item_id = item_methods.get_item_id(item_key)
-                html_string += "<div class='neutral-cell'>"
-                html_string += f"<div class='circle'>"
-                html_string += f"<img class='item-img' id='neutral-item' src='{img_host}/items/{item_key}.png' data_id='{item_id}' alt='{item_key}'>"
-                html_string += "<div class='tooltip' id='item-tooltip'></div></div></div>"
-
-            if match['aghanims_shard']:
-                image = f"<img class='item-img' id='aghanims-shard' src='https://cdn.cloudflare.steamstatic.com/apps/dota2/images/items/aghanims_shard_lg.png' data_id='609' data-hero=\"{match['hero']}\" alt='aghanims_shard'>"
-                shard_time = match['aghanims_shard'][0]['time']
-                overlay = f"<div class='overlay' id='shard-overlay'>{shard_time}</div>"
-                html_string += f"<div class='item-cell' id='aghanims-shard-cell'>"
-                html_string += image
-                html_string += overlay
-                html_string += "<div class='tooltip' id='shard-tooltip'></div>"
-                html_string += "</div>"
-
-            html_string += "</div></a>"
-        html_string += f"<div class='abilities' data-hero=\"{match['hero']}\">"
-        visited = []
-        for ability in match['abilities']:
-            talent_wrapper = ''
-            html_string += "<div class='ability-img-wrapper'>"
-            if ability['type'] == 'talent':
-                visited.append(ability)
-                talents = f"<div class='talents' data-name=\'{ability['key']}\'>"
-                for tal in visited:
-                    talents += talent_img(tal)
-                if len(talent_wrapper) == 0:
-                    talent_wrapper = ''
-                    talent_wrapper += talents
-                html_string += talent_str(ability, talent_wrapper)
-            else:
-                html_string += ability_str(ability, img_host, 'ability')
-        html_string += "</div>"
-
-        html_string += "<div class='draft'>"
-        html_string += draft_string(match, 'radiant', match['hero'])
-        html_string += "</div>"
-        html_string += draft_string(match, 'dire', match['hero'])
-        html_string += "</div>"
-
+        html_string += generate_item_string(template, img_host, match)
+        # ability
+        html_string += generate_ability_string(match, img_host)
+        html_string += generate_draft_string(match)
         role_file_path = f"/static/icons/{match['role']}.png"
         role_img = f"<img src='{role_file_path}'"
         if match['win'] == 0:
@@ -133,18 +67,73 @@ def generate_table(func_name, query, template, request):
         row_string.append(html_string)
         row_string.append(timeago.format(
             match['unix_time'], datetime.datetime.now()))
-        if func_name != 'player_get':
+        if func_name != 'player':
             row_string.append(
                 f"<a href=\"/player/{match['name']}\"><p class='stats'>{match['name']}</p></a>")
         else:
             row_string.append(
-                f"<a href='/hero/{match['hero']}'><i class='d2mh {match['hero']}'></i></a>")
+                f"<a href='/hero/{switcher(match['hero'])}'><i class='d2mh {match['hero']}'></i></a>")
         row_string.append(f"<i class='fas fa-copy' id='{match['id']}'></i>")
         row_string.append(
             f"<a href=\'?role={match['role']}\'><img src='{role_file_path}'/></a>")
         row_string += stats(match, template)
         result['data'].append(row_string)
     return result
+
+
+def generate_item_string(template, img_host, match):
+    html_string = "<div class='purchases'>"
+    if 'start' in template:
+        html_string += item_text_str(match['starting_items'],
+                                     img_host, 'starter', match['hero'])
+        html_string += "</div>"
+        html_string += "<div class='intermediate_items'>"
+        for item in match['items']:
+            intermediate_items = ['bottle', 'vanguard', 'hood_of_defiance', 'orb_of_corrosion',
+                                  'soul_ring', 'buckler', 'urn', 'fluffy_hat', 'wind_lace', 'infused_raindrop', 'crown', 'bracer', 'null_talisman', 'wraith_band',
+                                  'ring_of_basilius', 'headress', 'magic_wand']
+            consumables = ['tango', 'flask', 'ward_observer',
+                           'ward_sentry', 'smoke_of_deceit', 'enchanted_mango', 'clarity', 'tpscroll', 'dust']
+            item_key = item['key']
+            item_id = item_methods.get_item_id(item_key)
+            if type(item['time']) is not int or item['time'] > 600:
+                break
+            if item['key'] not in consumables and item['time'] < 600 and item['time'] > 0:
+                image = f"<img class='item-img' src='{img_host}/items/{item['key']}.png' data_id='{item_id}' alt='{item_key}'>"
+                overlay = f"<div class='overlay'>{str(datetime.timedelta(seconds=item['time']))}</div>"
+                html_string += "<div class='item-cell'>"
+                html_string += image
+                html_string += overlay
+                html_string += "<div class='tooltip' id='item-tooltip'></div>"
+                html_string += "</div>"
+        html_string += "</div></a>"
+
+    else:
+        html_string += item_text_str(match['final_items'],
+                                     img_host, None, match['hero'])
+        html_string += item_text_str(match['backpack'],
+                                     img_host, None, match['hero'])
+
+        if match['item_neutral']:
+            item_key = match['item_neutral']
+            item_id = item_methods.get_item_id(item_key)
+            html_string += "<div class='neutral-cell'>"
+            html_string += f"<div class='circle'>"
+            html_string += f"<img class='item-img' id='neutral-item' src='{img_host}/items/{item_key}.png' data_id='{item_id}' alt='{item_key}'>"
+            html_string += "<div class='tooltip' id='item-tooltip'></div></div></div>"
+
+        if match['aghanims_shard']:
+            image = f"<img class='item-img' id='aghanims-shard' src='https://cdn.cloudflare.steamstatic.com/apps/dota2/images/items/aghanims_shard_lg.png' data_id='609' data-hero=\"{match['hero']}\" alt='aghanims_shard'>"
+            shard_time = match['aghanims_shard'][0]['time']
+            overlay = f"<div class='overlay' id='shard-overlay'>{shard_time}</div>"
+            html_string += f"<div class='item-cell' id='aghanims-shard-cell'>"
+            html_string += image
+            html_string += overlay
+            html_string += "<div class='tooltip' id='shard-tooltip'></div>"
+            html_string += "</div>"
+
+        html_string += "</div></a>"
+    return html_string
 
 
 def item_text_str(items, img_host, type, hero):
@@ -165,6 +154,27 @@ def item_text_str(items, img_host, type, hero):
         else:
             html_string += "<div class='tooltip' id='item-tooltip'></div>"
         html_string += "</div>"
+    return html_string
+
+
+def generate_ability_string(match, img_host):
+    html_string = f"<div class='abilities' data-hero=\"{match['hero']}\">"
+    visited = []
+    for ability in match['abilities']:
+        talent_wrapper = ''
+        html_string += "<div class='ability-img-wrapper'>"
+        if ability['type'] == 'talent':
+            visited.append(ability)
+            talents = f"<div class='talents' data-name=\'{ability['key']}\'>"
+            for tal in visited:
+                talents += talent_img(tal)
+            if len(talent_wrapper) == 0:
+                talent_wrapper = ''
+                talent_wrapper += talents
+            html_string += talent_str(ability, talent_wrapper)
+        else:
+            html_string += ability_str(ability, img_host, 'ability')
+    html_string += "</div>"
     return html_string
 
 
@@ -204,6 +214,14 @@ def talent_img(talent):
         level = 25
     return f"<div class=\"lvl{level} {side}\"></div>"
 
+
+def generate_draft_string(match):
+    html_string = "<div class='draft'>"
+    html_string += draft_string(match, 'radiant', match['hero'])
+    html_string += "</div>"
+    html_string += draft_string(match, 'dire', match['hero'])
+    html_string += "</div>"
+    return html_string
 
 
 def draft_string(match, side, query):
