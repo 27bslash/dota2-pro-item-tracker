cb6f0b3870053eb0a5ec4b2d31b7538a503074eb
27bslash <greenpear68@yahoo.co.uk>
2021-10-03 19:13:27 +0100

    talent loaded after dom

---
+++ static/talent/talent_img.js
@@ -0,0 +1,47 @@
+const getTalents = async (search) => {
+  const url = `${window.location.origin}/files/${search}${window.location.search}`;
+  const req = await fetch(url);
+  data = await req.json();
+  return data;
+};
+const order_talents = (arr, n) => {
+  let temp = 0;
+  let ret = null;
+  for (let tal of arr) {
+    if (tal["level"] == n) {
+      if (tal["talent_count"] > temp) {
+        temp = tal["talent_count"];
+        let side = tal["slot"] % 2 != 0 ? "l-talent" : "r-talent";
+        ret = `lvl${n} ${side}`;
+      } else if (tal["talent_count"] == temp) {
+        return "EQUAL";
+      }
+    }
+  }
+  return ret;
+};
+window.addEventListener("load", (e) => {
+  createTalentImg();
+});
+const createTalentImg = async () => {
+  const talent_img = [];
+  const talents = await getTalents(`talent-data/${heroName}`);
+  for (let i = 10; i < 30; i += 5) {
+    talent_img.push(order_talents(talents, i));
+  }
+  const container = document.querySelector(".big-talent");
+  for (let [i, talent] of talent_img.entries()) {
+    if (talent === "EQUAL") {
+      const left = document.createElement("div");
+      left.setAttribute("class", `lvl${(i + 2) * 5} l-talent`);
+      const right = document.createElement("div");
+      right.setAttribute("class", `lvl${(i + 2) * 5} r-talent`);
+      container.appendChild(left);
+      container.appendChild(right);
+    } else {
+      const tal = document.createElement("div");
+      tal.setAttribute("class", talent);
+      container.appendChild(tal);
+    }
+  }
+};
---
+++ static/tooltip/big_talent_tooltip.js
@@ -0,0 +1,61 @@
+const bigTalent = async () => {
+  const container = document.querySelector("#talents");
+  const talents = await getTalents(`talent-data/${heroName}`);
+  for (let [i, talent] of talents.entries()) {
+    let center, barContainer, clss, col;
+    const slot = talent.slot;
+    if (slot % 2 === 0 || slot === 0) {
+      col = 3;
+      clss = "talent-right";
+    } else {
+      col = 1;
+      clss = "talent-left";
+    }
+    if ((i + 1) % 2 === 0) {
+      // center tooltip
+      center = document.createElement("div");
+      center.setAttribute("class", "talent-center");
+      level = document.createElement("div");
+      level.setAttribute("class", "talent-level");
+      levelText = document.createElement("p");
+      levelText.innerText = talent.level;
+      level.appendChild(levelText);
+      picks = document.createElement("div");
+      picks.setAttribute("class", "talent-picks");
+      picksText = document.createElement("p");
+      picksText.innerText = "Pick";
+      picks.appendChild(picksText);
+
+      center.appendChild(level);
+      center.appendChild(picks);
+    }
+    barContainer = document.createElement("div");
+    barContainer.setAttribute("class", clss);
+    barContainer.style.gridColumn = col;
+    let width = 0;
+    if (talent.total_pick_count > 0) {
+      width = (talent.talent_count / talent.total_pick_count) * 100;
+    }
+    talentText = document.createElement("p");
+    talentText.innerText = talent.key;
+    talentText.setAttribute("class", "talent-text");
+    talentBar = document.createElement("div");
+    talentBar.setAttribute("class", "talent-bar");
+    talentBar.style.width = width + "%";
+    talentBar.style.height = "45px";
+    valueTxt = document.createElement("p");
+    valueTxt.innerText = parseInt(Math.floor(width)) + "%";
+    barContainer.appendChild(talentText);
+    barContainer.appendChild(talentBar);
+    barContainer.appendChild(valueTxt);
+    if (center) {
+      container.appendChild(center);
+    }
+    if (barContainer) {
+      container.appendChild(barContainer);
+    }
+  }
+};
+window.addEventListener("load", (e) => {
+  bigTalent();
+});
