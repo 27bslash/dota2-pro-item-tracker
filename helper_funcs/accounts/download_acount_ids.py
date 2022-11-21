77e45a7a91804cef2b07fe63b5fc7a088a298a7d
27bslash <greenpear68@yahoo.co.uk>
2022-11-21 13:48:45 +0000

    Update download_acount_ids.py

--- helper_funcs/accounts/download_acount_ids.py
+++ helper_funcs/accounts/download_acount_ids.py
@@ -4,40 +4,40 @@ import time
 import re
 import traceback
 from helper_funcs.database.db import db
-from parsel import Selector
+# from parsel import Selector
 
 headers = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36"}
 
 
-def update_pro_accounts():
-    output = []
-    data = get_player_url_list()
-    if data == None:
-        print('pro tracker blocked')
-        return
-    url = 'http://www.dota2protracker.com/player/'
-    for player_name in data:
-        d = {}
-        try:
-            if db['account_ids'].find_one({'name': player_name}) is not None:
-                continue
-            print('out db', player_name)
-            req = requests.get(
-                f'{url}{player_name}', headers=headers)
-            text = req.text
-            selector = Selector(text=text)
-            acc_id = selector.xpath("//div[@class='player_stats']//a")[0]
-            acc_id = acc_id.css('a::attr(href)').extract()[0]
-            acc_id = re.sub(r"\D", '', acc_id, 0)
-            d['name'] = player_name
-            d['account_id'] = acc_id
-            time.sleep(1)
-            output.append(d)
-            db['account_ids'].find_one_and_update({'account_id': d['account_id']}, {"$set": {
-                'name': d['name'], 'account_id': d['account_id']}}, upsert=True)
-        except Exception as e:
-            print(traceback.format_exc(), player_name)
+# def update_pro_accounts():
+#     output = []
+#     data = get_player_url_list()
+#     if data == None:
+#         print('pro tracker blocked')
+#         return
+#     url = 'http://www.dota2protracker.com/player/'
+#     for player_name in data:
+#         d = {}
+#         try:
+#             if db['account_ids'].find_one({'name': player_name}) is not None:
+#                 continue
+#             print('out db', player_name)
+#             req = requests.get(
+#                 f'{url}{player_name}', headers=headers)
+#             text = req.text
+#             selector = Selector(text=text)
+#             acc_id = selector.xpath("//div[@class='player_stats']//a")[0]
+#             acc_id = acc_id.css('a::attr(href)').extract()[0]
+#             acc_id = re.sub(r"\D", '', acc_id, 0)
+#             d['name'] = player_name
+#             d['account_id'] = acc_id
+#             time.sleep(1)
+#             output.append(d)
+#             db['account_ids'].find_one_and_update({'account_id': d['account_id']}, {"$set": {
+#                 'name': d['name'], 'account_id': d['account_id']}}, upsert=True)
+#         except Exception as e:
+#             print(traceback.format_exc(), player_name)
 
 
 # get_account_ids()
