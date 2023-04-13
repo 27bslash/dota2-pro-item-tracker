d5ee1698313351094cbbd84eee27c79fc1addcc5
27bslash <greenpear68@yahoo.co.uk>
2023-04-13 14:53:06 +0100

    account ids now use updated json

--- helper_funcs/accounts/download_acount_ids.py
+++ helper_funcs/accounts/download_acount_ids.py
@@ -4,7 +4,11 @@ import time
 import re
 import traceback
 from helper_funcs.database.db import db
-from parsel import Selector
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 headers = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36"}
@@ -12,6 +16,9 @@ headers = {
 
 def update_pro_accounts():
     output = []
+    # with open('helper_funcs/accounts/accounts.json', 'r') as f:
+    #     data = json.load(f)
+    #     data = data['players']
     data = get_player_url_list()
     if data == None:
         print('pro tracker blocked')
@@ -26,10 +33,15 @@ def update_pro_accounts():
             req = requests.get(
                 f'{url}{player_name}', headers=headers)
             text = req.text
-            selector = Selector(text=text)
-            acc_id = selector.xpath("//div[@class='player_stats']//a")[0]
-            acc_id = acc_id.css('a::attr(href)').extract()[0]
-            acc_id = re.sub(r"\D", '', acc_id, 0)
+            with open('test.html', 'r', encoding='utf-8') as f:
+                soup = BeautifulSoup(text, 'html.parser')
+                link = soup.find('a', href=re.compile(r'stratz.com/player/'))
+                if link is None:
+                    print(player_name, 'not found')
+                    continue
+                acc_id = re.search(r"\d+", link['href'])
+                if acc_id:
+                    acc_id = acc_id.group(0)
             d['name'] = player_name
             d['account_id'] = acc_id
             time.sleep(1)
@@ -42,8 +54,12 @@ def update_pro_accounts():
 
 # get_account_ids()
 def get_player_url_list():
+    player_url = get_player_url()
+    if not player_url:
+        player_url = 'http://www.dota2protracker.com/static/search_items_291022.json'
+        print('no url using default')
     req = requests.get(
-        'http://www.dota2protracker.com/static/search_items_595274230.json', headers=headers)
+        'http://www.dota2protracker.com/static/search_items_291022.json', headers=headers)
     if req.status_code != 200:
         print(f'status:{req.status_code} blocked')
         return None
@@ -54,6 +70,43 @@ def get_player_url_list():
     return [k for k in players]
 
 
+def get_player_url() -> str | None:
+    capabilities = DesiredCapabilities.CHROME
+    capabilities["goog:loggingPrefs"] = {'performance': 'ALL'}
+    chrome = webdriver.Chrome(service=Service(
+        ChromeDriverManager().install()), desired_capabilities=capabilities)
+
+    # chrome.get('https://dota2itemtracker.vercel.app/')
+    chrome.get('http://www.dota2protracker.com')
+
+    time.sleep(3)
+    logs = chrome.get_log('performance')
+    for log in logs:
+        log = json.loads(log['message'])
+        if log['message']['method'] != 'Network.responseReceived':
+            continue
+        if log['message']['params']['response']['mimeType'] != 'application/json':
+            continue
+        url = log['message']['params']['response']['url']
+        if 'search_items' in url:
+            return url
+    # networkScript = """
+    #     let performance = window.performance || window.webkitPerformance || {};
+    #     let network = performance.getEntries() || {};
+    #     return network;
+    # """
+    # networkRequests = chrome.execute_script(networkScript)
+    # URLs = [request['name']
+    #         for request in networkRequests if 'json' in request['name']]
+
+    # your logic on what to do if request is found
+    # print(URLs)
+    # print(networkRequests)
+
+    # time.sleep(10)
+    # logs = chrome.get_log('performance')
+    # print(logs)
+    pass
 # def convert_account_str_json():
 #     """returns raw accounts string converted to json"""
 #     with open('raw_accounts_string.txt', 'r') as f:
@@ -74,4 +127,4 @@ def get_player_url_list():
 #     return re.sub(regex, '', string)
 
 if __name__ == '__main__':
-    update_pro_accounts()
+    get_player_url()
