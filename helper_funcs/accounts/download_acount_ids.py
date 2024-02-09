09d2db5c6694ad83daa4db4baf43d1bf3e9a09e5
27bslash <greenpear68@yahoo.co.uk>
2024-02-09 18:06:50 +0000

    updated download_account_ids

--- helper_funcs/accounts/download_acount_ids.py
+++ helper_funcs/accounts/download_acount_ids.py
@@ -9,87 +9,116 @@ from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
 
 headers = {
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36"}
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36"
+}
 
 
-def update_pro_accounts():
+def update_pro_accounts(testing=False):
     output = []
     # with open('helper_funcs/accounts/accounts.json', 'r') as f:
     #     data = json.load(f)
     #     data = data['players']
-    data = get_player_url_list()
+    if testing:
+        with open("helper_funcs/accounts/new_accs.json", "r") as f:
+            data = json.load(f)['players']
+    else:
+        data = get_player_url_list()
     if data == None:
-        print('pro tracker blocked')
+        print("pro tracker blocked")
         return
-    url = 'http://www.dota2protracker.com/player/'
-    for player_name in data:
+    url = "http://www.dota2protracker.com/player/"
+    for doc in data:
         d = {}
         try:
-            if db['account_ids'].find_one({'name': player_name}) is not None:
+            if type(doc) == dict:
+                d["name"] = doc["name"]
+                d["account_id"] = doc["account_id"]
+            if db["account_ids"].find_one({"name": doc["name"]}):
                 continue
-            print('out db', player_name)
-            req = requests.get(
-                f'{url}{player_name}', headers=headers)
-            text = req.text
-            with open('test.html', 'r', encoding='utf-8') as f:
-                soup = BeautifulSoup(text, 'html.parser')
-                link = soup.find('a', href=re.compile(r'stratz.com/player/'))
-                if link is None:
-                    print(player_name, 'not found')
+            if "account_id" not in d:
+                if db["account_ids"].find_one({"name": doc}):
                     continue
-                acc_id = re.search(r"\d+", link['href'])
+                req = requests.get(f"{url}{doc}", headers=headers)
+                text = req.text
+                soup = BeautifulSoup(text, "html.parser")
+                link = soup.find("a", href=re.compile(r"stratz.com/player/"))
+                if link is None:
+                    print(doc, "not found")
+                    continue
+                acc_id = re.search(r"\d+", link["href"])
                 if acc_id:
                     acc_id = acc_id.group(0)
-            d['name'] = player_name
-            d['account_id'] = acc_id
+                d["name"] = doc
+                d["account_id"] = acc_id
+            print("out db", d['name'], d['account_id'])    
             time.sleep(1)
             output.append(d)
-            db['account_ids'].find_one_and_update({'account_id': d['account_id']}, {"$set": {
-                'name': d['name'], 'account_id': d['account_id']}}, upsert=True)
+            db["account_ids"].find_one_and_update(
+                {"account_id": d["account_id"]},
+                {"$set": {"name": d["name"], "account_id": d["account_id"]}},
+                upsert=True,
+            )
         except Exception as e:
-            print(traceback.format_exc(), player_name)
+            print(traceback.format_exc(), doc)
+    print(output)
 
 
 # get_account_ids()
 def get_player_url_list():
     player_url = get_player_url()
     if not player_url:
-        player_url = 'http://www.dota2protracker.com/static/search_items_291022.json'
-        print('no url using default')
-    req = requests.get(
-        'http://www.dota2protracker.com/static/search_items_291022.json', headers=headers)
+        player_url = "https://dota2protracker.com/_get/search"
+        print("no url using default")
+    req = requests.get(player_url, headers=headers)
     if req.status_code != 200:
-        print(f'status:{req.status_code} blocked')
+        print(f"status:{req.status_code} blocked")
         return None
-    if 'players' not in req.text:
-        print('misc blocked')
+    if "players" not in req.text:
+        print("misc blocked")
         return None
-    players = json.loads(req.text)['players']
+    players = json.loads(req.text)["players"]
     return [k for k in players]
 
 
 def get_player_url() -> str | None:
-    capabilities = DesiredCapabilities.CHROME
-    capabilities["goog:loggingPrefs"] = {'performance': 'ALL'}
-    chrome = webdriver.Chrome(service=Service(
-        ChromeDriverManager().install()), desired_capabilities=capabilities)
+    capabilities = DesiredCapabilities.CHROME.copy()
+    capabilities["goog:loggingPrefs"] = {"performance": "ALL"}
+    chrome = webdriver.Chrome(
+        ChromeDriverManager().install(), desired_capabilities=capabilities
+    )
 
     # chrome.get('https://dota2itemtracker.vercel.app/')
-    chrome.get('http://www.dota2protracker.com')
-
+    chrome.get("http://www.dota2protracker.com")
+    button_available = WebDriverWait(chrome, 40).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "fc-cta-consent"))
+    )
+    if button_available:
+        time.sleep(3)
+        try:
+            button_available.click()
+        except NoSuchElementException as e:
+            print("no cookie consent", e)
     time.sleep(3)
-    logs = chrome.get_log('performance')
+    logs = chrome.get_log("performance")
     for log in logs:
-        log = json.loads(log['message'])
-        if log['message']['method'] != 'Network.responseReceived':
+        log = json.loads(log["message"])
+        if log["message"]["method"] != "Network.responseReceived":
             continue
-        if log['message']['params']['response']['mimeType'] != 'application/json':
+        if log["message"]["params"]["response"]["mimeType"] != "application/json":
             continue
-        url = log['message']['params']['response']['url']
-        if 'search_items' in url:
+        url = log["message"]["params"]["response"]["url"]
+        print(url)
+        if "search" in url:
+            chrome.quit()
             return url
+    chrome.quit()
+
     # networkScript = """
     #     let performance = window.performance || window.webkitPerformance || {};
     #     let network = performance.getEntries() || {};
@@ -107,6 +136,8 @@ def get_player_url() -> str | None:
     # logs = chrome.get_log('performance')
     # print(logs)
     pass
+
+
 # def convert_account_str_json():
 #     """returns raw accounts string converted to json"""
 #     with open('raw_accounts_string.txt', 'r') as f:
@@ -126,5 +157,5 @@ def get_player_url() -> str | None:
 #     regex = r"\[.*|NEW"
 #     return re.sub(regex, '', string)
 
-if __name__ == '__main__':
-    get_player_url()
+if __name__ == "__main__":
+    update_pro_accounts(True)
