936ee968c31b7b45884354502ec12f539c1d00b8
27bslash <greenpear68@yahoo.co.uk>
2025-08-16 22:29:58 +0100

    urls updated for pro games

--- helper_funcs/url.py
+++ helper_funcs/url.py
@@ -2,6 +2,8 @@ import time
 import traceback
 import requests
 from helper_funcs.database.db import db
+from helper_funcs.hero import Hero
+
 
 hero_urls = db["urls"]
 hero_output = db["heroes"]
@@ -9,23 +11,33 @@ parse = db["parse"]
 dead_games = db["dead_games"]
 
 
-def get_urls(hero_name):
+def get_urls(hero_id: int, pro=False):
+    query = (
+        {"hero": hero_id, "pro": True, 'mmr': {"$gt": 0}}
+        if pro
+        else {"heroes": hero_id, 'mmr': {"$gt": 0}}
+    )
+    data = hero_urls.find(query)
+
     urls = []
-    data = hero_urls.find({"hero": hero_name})
-    urls = [
-        match["id"]
-        for match in data
-        if hero_output.find_one(
-            {"hero": hero_name, "id": match["id"], "parsed": {"$ne": False}}
+
+    for match in list(data):
+        match_id = match["id"]
+        hero_name = Hero().hero_name_from_hero_id(hero_id)
+        hero_output_search = hero_output.find_one(
+            {"hero": hero_name, "id": match_id, "parsed": {"$ne": False}}
         )
-        is None
-        and parse.find_one({"id": match["id"]}) is None
-        and (
-            dead_games.find_one({"id": match["id"]}) is None
-            or dead_games.find_one({"id": match["id"], "count": {"$lte": 20}})
-        )
-    ]
-    # return list(reversed(urls[slice(0, 60)]))
+        if hero_output_search:
+            continue
+
+        if parse.find_one({"id": match_id}):
+            continue
+
+        if dead_games.find_one({"id": match_id, "count": {"$gt": 20}}):
+            continue
+
+        urls.append(match_id)
+
     return list(reversed(urls))
 
 
@@ -34,6 +46,7 @@ def delete_old_urls(max_age=690000):
     for d in list(data)[::-1]:
         time_since = time.time() - d["time_stamp"]
         # 8 days old
+        print(time_since)
         if time_since > max_age:
             collections = db.list_collection_names()
             for collection in collections:
@@ -58,14 +71,15 @@ def parse_request():
             time.sleep(1)
         except Exception:
             break
-        if i >= 60:
+        if i >= 600:
             break
         parse.delete_many({"id": match["id"]})
 
 
-
 if __name__ == "__main__":
-    d = list(hero_output.find({"parsed": {"$ne": False}}))
-    print(get_urls("jakiro"), len(d))
-
-    # delete_old_urls()
+    # print(get_urls(51, pro=True))
+    target = 8379013910
+    list = get_urls(47)
+    print(list)
+    if target in list:
+        print("found")
