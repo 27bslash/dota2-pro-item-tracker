f984ac972d8551ed37d2653e3cbb8ee153b15dc2
27bslash <greenpear68@yahoo.co.uk>
2025-08-16 22:41:57 +0100

    updated build script

--- build.py
+++ build.py
@@ -3,24 +3,20 @@ import PyInstaller.__main__
 from swinlnk.swinlnk import SWinLnk
 
 
+# TODO investigate using my own parser to avoid api limits
 def create_exe(pyfile: str, noconsole=False):
     if noconsole:
-        PyInstaller.__main__.run([
-            pyfile,
-            '--onefile', '--noconsole'
-        ])
+        PyInstaller.__main__.run([pyfile, "--onefile", "--noconsole"])
     else:
-        PyInstaller.__main__.run([
-            pyfile,
-            '--onefile'
-        ])
+        PyInstaller.__main__.run([pyfile, "--onefile"])
 
 
 def shrtcut(shortcut_name, exe_name):
-    import os
     from win32com.client import Dispatch
+
     startup = os.path.expandvars(
-        r'%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup')
+        r"%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
+    )
     shortcut_path = f"{startup}\\{shortcut_name}"
 
     path = shortcut_path  # Path to be saved (shortcut)
@@ -28,32 +24,38 @@ def shrtcut(shortcut_name, exe_name):
     target = f"{os.path.abspath(f'./dist/{exe_name}')}"
     work_dir = f"{os.path.abspath('./dist')}"  # The parent folder of your file
 
-    shell = Dispatch('WScript.Shell')
+    shell = Dispatch("WScript.Shell")
     shortcut = shell.CreateShortCut(path)
     shortcut.Targetpath = target
     shortcut.WorkingDirectory = work_dir
     shortcut.save()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    import traceback
+
     try:
-        PyInstaller.__main__.run([
-            'clock.py',
-            '--onefile',
-        ])
+        PyInstaller.__main__.run(
+            [
+                "clock.py",
+                "--onefile",
+            ]
+        )
         # create_exe('clock.py')
-        shrtcut('pro-item-tracker.lnk', 'clock.exe')
-    except Exception as e:
-        import traceback
+        shrtcut("pro-item-tracker.lnk", "clock.exe")
+    except Exception:
         print(traceback.format_exc())
     try:
-        create_exe('hero_guides/update_builds/update_builds.py')
-        shrtcut('update_hero_guides.lnk', 'update_builds.exe')
-    except Exception as e:
-        import traceback
+        create_exe("hero_guides/update_builds/update_builds.py")
+        shrtcut("update_hero_guides.lnk", "update_builds.exe")
+    except Exception:
         print(traceback.format_exc())
-    create_exe()
-    shrtcut()
+    try:
+        create_exe("parse_personal_matches/get_games.py", noconsole=True)
+        shrtcut("opendota_updater.lnk", "get_games.exe")
+    except Exception:
+        print(traceback.format_exc())
+        pass
     # move_to_startup()
     # print('done')
     # sys.exit(0)
